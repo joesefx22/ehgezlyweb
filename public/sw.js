64331d14a77e334d4d823d745b12ec66a7ac27aa
joesefx22 <joesefmohamed98@gmail.com>
2025-11-26 11:20:31 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+// public/sw.js
+const CACHE_NAME = 'ehgzly-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
