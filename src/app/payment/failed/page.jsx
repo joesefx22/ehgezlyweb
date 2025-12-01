6a57b68b511e99a60fbeff6349db6c664cba8004
joesefx22 <joesefmohamed98@gmail.com>
2025-12-01 19:47:06 +0200

    Create page.jsx

---
+++ src/app/payment/failed/page.jsx
@@ -0,0 +1,8 @@
+export default function PaymentFailed() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-red-600 text-3xl font-bold mb-4">فشل الدفع ❌</h1>
+      <p className="text-gray-700">حدثت مشكلة أثناء الدفع… حاول مرة أخرى.</p>
+    </div>
+  );
+}
