d18eccc3dae3b627475631baf66acd00a6454b47
joesefx22 <joesefmohamed98@gmail.com>
2025-12-02 15:56:26 +0200

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,7 @@
+export default function sitemap() {
+  return [
+    { url: "https://ehgezly.com/", lastModified: new Date() },
+    { url: "https://ehgezly.com/about", lastModified: new Date() },
+    { url: "https://ehgezly.com/contact", lastModified: new Date() },
+  ];
+}
